3a6fd0ba440fa22871e0173cfd23eff23866e749
Max Berger <maxberger@Maxs-MacBook-Pro.local>
2017-02-09 10:28:44 -0800

    Complete stack.js in prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,31 @@
 var Stack = function() {
-
+  var instance = Object.create(stackMethods);
+  instance.sizeCount = 0;
+  instance.storage = {};
+  instance.temp = 0;
+  instance.deleteCount = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    if (this.sizeCount < 0) {
+      this.sizeCount = 0;
+    }
+    return this.sizeCount;
+  },
+
+  push: function(value) {
+    this.storage[this.sizeCount] = value;
+    this.sizeCount++;
+  },
+
+  pop: function() {
+    this.sizeCount--;
+    this.temp = this.storage[this.sizeCount];
+    delete this.storage[this.sizeCount];
+    return this.temp;
+  }
+};
 
 
